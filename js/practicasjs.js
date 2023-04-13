78843fce97cae7aec16b2fd9aa48f96aea61c099
Extermy <112786468+Extermy-23@users.noreply.github.com>
2023-04-13 14:46:57 -0500

    v1.5.3

---
+++ js/practicasjs.js
@@ -0,0 +1,13 @@
+let quest = confirm("Desea ser redirecionado a la pagina de inicio?")
+
+switch (quest) {
+    case (true):
+            
+        window.location.href = "../index.html"
+
+        break;
+
+    default:   
+
+        break;
+}
